10301e3a16623ee835bfa85b11d986b058141580
kalay20 <kalay20@gmail.com>
2015-11-22 21:48:21 -0800

    dir script

--- s2/trace_list.sh
+++ s2/trace_list.sh
@@ -3,17 +3,22 @@
 
 
 wd_start=${wd_start:-0}
-wd_end=${wd_end:-6}
-wd_bool_default=(1 0 0 0 0 0)
+wd_end=${wd_end:-11}
+wd_bool_default=(1 0 0 0 0 0 0 0 0 0 0)
 wd_bool=("${wd_bool[@]:-${wd_bool_default[@]}}")
-workload_prefix=( mse msn msl usr1 src10 synf )
+workload_prefix=( ex msl msn syf syn 2cache 3cache 4cache 5cache 6cache 7cache )
 workload_origin_path=( \
 	/home/r01/gengyouchen/ssd_trace/ms_exchange_server.trace \
-	/home/r01/gengyouchen/ssd_trace/msn_fs.trace \
 	/home/r01/gengyouchen/ssd_trace/ms_live_maps.trace \
-	/home/r01/gengyouchen/ssd_trace/MSRC-io-traces-ascii/usr_1.trace \
-	/home/r01/gengyouchen/ssd_trace/MSRC-io-traces-ascii/src1_0.trace \
+	/home/r01/gengyouchen/ssd_trace/msn_fs.trace \
 	/home/r01/gengyouchen/ssd_trace/synthetic_overload_full.trace \
+	/home/r01/gengyouchen/ssd_trace/synthetic_overload.trace \
+	/home/r01/gengyouchen/ssd_trace/SieveDTrace-parsed-2/2.cache.trace \
+	/home/r01/gengyouchen/ssd_trace/SieveDTrace-parsed-2/3.cache.trace \
+	/home/r01/gengyouchen/ssd_trace/SieveDTrace-parsed-2/4.cache.trace \
+	/home/r01/gengyouchen/ssd_trace/SieveDTrace-parsed-2/5.cache.trace \
+	/home/r01/gengyouchen/ssd_trace/SieveDTrace-parsed-2/6.cache.trace \
+	/home/r01/gengyouchen/ssd_trace/SieveDTrace-parsed-2/7.cache.trace \
 )
 #workload_burst_path_prefix=/home/r03/sin19682004/test/simulator2/trace
 
